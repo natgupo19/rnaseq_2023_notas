3574b3915962fbfb02e9e2c6b746ee543bd3eb25
[natgupo19] <[natgupo@lcg.unam.mx]>
2023-02-03 12:40:26 -0600

    Resolucion del ejercicio expresion diferencial

--- R/06_notas_modelos_e.R
+++ R/06_notas_modelos_e.R
@@ -273,167 +273,6 @@ dev.off()
 
 
 
-
-## ?model.matrix
-mat <- with(trees, model.matrix(log(Volume) ~ log(Height) + log(Girth)))
-mat
-## tenemos tres variables en la regresion lineal
-
-
-## Resumen de la regresion lineal
-summary(lm(log(Volume) ~ log(Height) + log(Girth), data = trees))
-
-
-## Paquete ExploreModelMatrix
-## Aplicacion interactiva y ayuda a las personas a interpretar sus modelos estadisticos
-## Crea una tablita que nos ayuda a interpretar los coeficientes
-
-## Ejemplo 1
-## Datos de ejemplo
-(sampleData <- data.frame(
-  genotype = rep(c("A", "B"), each = 4),
-  treatment = rep(c("ctrl", "trt"), 4)
-))
-
-## Creemos las imágenes usando ExploreModelMatrix
-vd <- ExploreModelMatrix::VisualizeDesign(
-  sampleData = sampleData,
-  designFormula = ~ genotype + treatment,
-  textSizeFitted = 4
-)
-
-## Veamos las imágenes
-cowplot::plot_grid(plotlist = vd$plotlist)
-
-
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
-BiocManager::install("ExploreModelMatrix")
-
-
-## Usaremos shiny otra vez
-app <- ExploreModelMatrix(
-  sampleData = sampleData,
-  designFormula = ~ genotype + treatment
-)
-if (interactive()) shiny::runApp(app)
-
-
-
-## EJERCICIO
-
-## Le restamos la primera columna a la segunda para obtener ResponseResistant.treatmentpre
-
-## Es necesario el cero para que no se tome batch1 como referencia
-
-
-
-
-## DATOS DE SRP045638
-
-library("recount3")
-
-options(recount3_url = "https://recount-opendata.s3.amazonaws.com/recount3/release")
-
-human_projects <- available_projects()
-
-
-rse_gene_SRP045638 <- create_rse(
-  subset(
-    human_projects,
-    project == "SRP045638" & project_type == "data_sources"
-  )
-)
-
-assay(rse_gene_SRP045638, "counts") <- compute_read_counts(rse_gene_SRP045638)
-
-
-rse_gene_SRP045638$sra.sample_attributes[1:3]
-
-
-rse_gene_SRP045638$sra.sample_attributes <- gsub("dev_stage;;Fetal\\|", "", rse_gene_SRP045638$sra.sample_attributes)
-rse_gene_SRP045638$sra.sample_attributes[1:3]
-
-rse_gene_SRP045638 <- expand_sra_attributes(rse_gene_SRP045638)
-
-colData(rse_gene_SRP045638)[
-  ,
-  grepl("^sra_attribute", colnames(colData(rse_gene_SRP045638)))
-]
-
-
-
-## Pasar de character a nuemric o factor
-rse_gene_SRP045638$sra_attribute.age <- as.numeric(rse_gene_SRP045638$sra_attribute.age)
-rse_gene_SRP045638$sra_attribute.disease <- factor(rse_gene_SRP045638$sra_attribute.disease)
-rse_gene_SRP045638$sra_attribute.RIN <- as.numeric(rse_gene_SRP045638$sra_attribute.RIN)
-rse_gene_SRP045638$sra_attribute.sex <- factor(rse_gene_SRP045638$sra_attribute.sex)
-
-## Resumen de las variables de interés
-summary(as.data.frame(colData(rse_gene_SRP045638)[
-  ,
-  grepl("^sra_attribute.[age|disease|RIN|sex]", colnames(colData(rse_gene_SRP045638)))
-]))
-
-
-## Encontraremos diferencias entre muestra prenatalas vs postnatales
-rse_gene_SRP045638$prenatal <- factor(ifelse(rse_gene_SRP045638$sra_attribute.age < 0, "prenatal", "postnatal"))
-table(rse_gene_SRP045638$prenatal)
-
-
-## http://rna.recount.bio/docs/quality-check-fields.html
-rse_gene_SRP045638$assigned_gene_prop <- rse_gene_SRP045638$recount_qc.gene_fc_count_all.assigned / rse_gene_SRP045638$recount_qc.gene_fc_count_all.total
-summary(rse_gene_SRP045638$assigned_gene_prop)
-
-
-with(colData(rse_gene_SRP045638), plot(assigned_gene_prop, sra_attribute.RIN))
-
-## Hm... veamos si hay una diferencia entre los grupos
-with(colData(rse_gene_SRP045638), tapply(assigned_gene_prop, prenatal, summary))
-
-## Guardemos nuestro objeto entero por si luego cambiamos de opinión
-rse_gene_SRP045638_unfiltered <- rse_gene_SRP045638
-
-## Eliminemos a muestras malas
-hist(rse_gene_SRP045638$assigned_gene_prop)
-
-
-table(rse_gene_SRP045638$assigned_gene_prop < 0.3)
-
-rse_gene_SRP045638 <- rse_gene_SRP045638[, rse_gene_SRP045638$assigned_gene_prop > 0.3]
-
-## Calculemos los niveles medios de expresión de los genes en nuestras
-## muestras.
-## Ojo: en un análisis real probablemente haríamos esto con los RPKMs o CPMs
-## en vez de las cuentas.
-gene_means <- rowMeans(assay(rse_gene_SRP045638, "counts"))
-summary(gene_means)
-
-## RIN: entre más alto, menos RNAs rotos
-
-
-## Eliminamos genes
-rse_gene_SRP045638 <- rse_gene_SRP045638[gene_means > 0.1, ]
-
-## Dimensiones finales
-dim(rse_gene_SRP045638)
-
-## Porcentaje de genes que retuvimos
-round(nrow(rse_gene_SRP045638) / nrow(rse_gene_SRP045638_unfiltered) * 100, 2)
-
-
-
-## NORMALIZACIÓN DE DATOS
-
-library("edgeR") # BiocManager::install("edgeR", update = FALSE)
-dge <- DGEList(
-  counts = assay(rse_gene_SRP045638, "counts"),
-  genes = rowData(rse_gene_SRP045638)
-)
-dge <- calcNormFactors(dge)
-
-
 ## EXPRESION DIFERENCIAL
 
 ## Explorariamos mas los datos para revisar que no haya otros problemas con las muestras y para explorar la relación entre nuestras variables.
@@ -541,10 +380,33 @@ col.sex <- as.character(col.sex)
 plotMDS(vGene$E, labels = df$Sex, col = col.sex)
 
 
+## EJERCICIO
 
-## Ejercicio
-## Hagamos un heatmap
-library("pheatmap")
+## Tenemos que usar gene_id y gene_name
+rowRanges(rse_gene_SRP045638)
+
+## Alternativamente, podriamos haber usado de_results
+head(de_results, n = 3)
+
+## Es la misma información
+identical(rowRanges(rse_gene_SRP045638)$gene_id, de_results$gene_id)
+identical(rowRanges(rse_gene_SRP045638)$gene_name, de_results$gene_name)
+
+## Guardemos los IDs de nuestros 50 genes
+nombres_originales <- rownames(exprs_heatmap)
+
+## Con match() podemos encontrar cual es cual
+rownames(exprs_heatmap) <- rowRanges(rse_gene_SRP045638)$gene_name[
+  match(rownames(exprs_heatmap), rowRanges(rse_gene_SRP045638)$gene_id)
+]
+
+## Vean que tambien podriamos haber usado rank()
+identical(
+  which(rank(de_results$adj.P.Val) <= 50),
+  match(nombres_originales, rowRanges(rse_gene_SRP045638)$gene_id)
+)
+
+## Por último podemos cambiar el valor de show_rownames de FALSE a TRUE
 pheatmap(
   exprs_heatmap,
   cluster_rows = TRUE,
@@ -552,4 +414,16 @@ pheatmap(
   show_rownames = TRUE,
   show_colnames = FALSE,
   annotation_col = df
+)
+
+
+## Versión con centering y scaling en los renglones (los genes)
+pheatmap::pheatmap(
+  exprs_heatmap,
+  cluster_rows = TRUE,
+  cluster_cols = TRUE,
+  show_rownames = TRUE,
+  show_colnames = FALSE,
+  annotation_col = df,
+  scale = "row"
 )
